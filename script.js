a53c90109f88a7d1f06c3b8effaaba3930d5dad4
Suryashivaji <devilsurya265@gmail.com>
2023-11-25 11:10:17 +0530

    completed

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 const display =document.querySelector(".display");
 const buttons =document.querySelectorAll("button");
 const specialChar =["%","*","/","-","=","+"]
-
-const alph = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 let output = "";
 const calculate = (btnValue) => {
 if(btnValue=== "=" && output!== ""){
